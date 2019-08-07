f43c291a3b4efe5ebf4fa4751206ded866e31a45
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-08-07 17:16:03 -0400

    sortStack algorithim

--- extensions.js
+++ extensions.js
@@ -3,17 +3,18 @@
 
 // Extension 4
 
-function union(arrays) {
-  const args = [...arguments]
-  let results = new Set(args[0]);
-  for (let i = 1; i < args.length; i++) {
-    for (let j = 0; j < args[i].length; j++) {
-      results.add(args[i][j]);
-    }
-  }
-  //console.log(arguments[1])
-  return results;
-}
+//function union(arrays) {
+  //const args = [...arguments]
+  //let results = new Set(args[0]);
+  //for (let i = 1; i < args.length; i++) {
+    //for (let j = 0; j < args[i].length; j++) {
+      //results.add(args[i][j]);
+    //}
+  //}
+  ////console.log(arguments[1])
+  //return results;
+//}
 
-console.log(union([5,10,15], [15,88,1,5,7],[100, 15, 10, 1, 5]));
-// should log: [5,10,15,88,1,7,100]
+//console.log(union([5,10,15], [15,88,1,5,7],[100, 15, 10, 1, 5]));
+//// should log: [5,10,15,88,1,7,100]
+
